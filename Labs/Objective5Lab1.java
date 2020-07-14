ab0766c303ca875a673a8e038623f29d692cec1d
Jourdan Rentschler <jrentschler3@gmail.com>
2020-07-14 16:39:09 -0600

    complete objective5lab1

--- Labs/Objective5Lab1.java
+++ Labs/Objective5Lab1.java
@@ -12,14 +12,14 @@ public class Objective5Lab1 {
         Your solution goes here
       */
       num1 = 5;
-      System.out.println("num1 is greater than num2");
+      System.out.println(num1 + " is greater than " + num2);
     }
     else if(num2 > num1) {
       /*
         Your solution goes here
       */
       num2 = 15;
-      System.out.println("num2 is greater than num1");
+      System.out.println(num2 + " is greater than " + num1);
     }
   }
 }
