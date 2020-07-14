c59e9222e2f62e23671b7bcf059f942887123078
Jourdan Rentschler <jrentschler3@gmail.com>
2020-07-14 17:31:30 -0600

    complete objective5lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int userNum;
+
+      System.out.println("Please enter a number: ");
+
+      /*
+        Your solution goes here
+      */
+      userNum = scanner.nextInt();
+      System.out.println("The number is positive");
+
+      scanner.close();
+  }
+}
