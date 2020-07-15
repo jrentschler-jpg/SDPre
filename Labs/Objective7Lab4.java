5a1f56f593eed233274d5923e3a21ee262a58caa
Jourdan Rentschler <jrentschler3@gmail.com>
2020-07-15 14:42:24 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 0;
+    int sum = 210;
+
+
+    while(counter > 20);
+    counter = counter + 1;
+
+    System.out.println(counter + sum);
+
+  }
+}
