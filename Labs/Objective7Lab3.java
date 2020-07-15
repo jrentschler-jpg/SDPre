12d0ea601d64cdd1a553f9ccd8c202fb956648e9
Jourdan Rentschler <jrentschler3@gmail.com>
2020-07-15 14:09:26 -0600

    complete objective7lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,26 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    /*
+      Your solution goes here
+    */
+    while(counter <= 19) {
+      counter = counter + 1;
+      System.out.println(counter);
+
+    for (int i=0; i<=19; i++) {
+
+      if(i%2==1) {
+        System.out.println(i + " is odd ");
+      }
+    }
+    for (int i=0; i<19; i++) {
+      if(i%2==0) {
+        System.out.println(i + " is even ");
+      }
+    }
+
+    }
+  }
+}
