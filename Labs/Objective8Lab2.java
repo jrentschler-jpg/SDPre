817f86385b84f47e0e1e5613fbdd5050d08b0101
Jourdan Rentschler <jrentschler3@gmail.com>
2020-07-16 12:56:46 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int num = 20, sum = 210;
+    for(int i=1; i<=num; ++i){
+      sum += i;
+      System.out.println(sum);
+    }
+  }
+}
